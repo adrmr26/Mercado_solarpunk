82f23ec3b7f6ab23250f5781cf09cedc171ab628
Adrian Molina <adrmr26@gmail.com>
2023-06-12 10:15:30 -0600

    Actualización de encargado.c

--- encargado.c
+++ encargado.c
@@ -1,40 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
+
+/* Semáforos */
 #include <semaphore.h>
 
-#define N_THREADS 5
+/* Memoria compartida */
+#include <sys/mman.h>
+#include <sys/stat.h>       
+#include <fcntl.h>           
 
-int contador = 0;
-sem_t semaforo;
+/* Generales */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-void *funcion_hilo(void *arg) {
-    int id = *(int *) arg;
-    printf("Hilo %d iniciado\n", id);
-    for (int i = 0; i < 8; i++) {
-        sem_wait(&semaforo); // esperar a que el semáforo sea mayor que cero
-        contador++; // sección crítica
-        printf("Hilo %d: contador = %d\n", id, contador);
-        sem_post(&semaforo); // incrementar el semáforo en uno
-        sleep(1); // esperar un segundo
+/* FORK */
+#include <sys/types.h>
+#include <unistd.h>
+
+/* Wait de los procesos */
+#include <sys/wait.h>
+
+/* srand */
+#include <time.h>
+
+/* structs.h*/
+#include "structs.h"
+
+/* DEFINES */
+#define MC "/memoria_compartida_almacen"
+#define MAX_PRODUCTOS 3
+
+void crearMemoriaCompartida(int *fd_shm) {
+    // Crear y configurar la memoria compartida
+    *fd_shm = shm_open(MC, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+    if (*fd_shm == -1) {
+        perror("Error al crear la memoria compartida");
+        exit(EXIT_FAILURE);
     }
-    printf("Hilo %d finalizado\n", id);
-    pthread_exit(NULL);
+    if (ftruncate(*fd_shm, MAX_PRODUCTOS * sizeof(Producto)) == -1) {
+        perror("Error al ajustar el tamaño de la memoria compartida");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void borrarMemoriaCompartida(int fd_shm) {
+    // Liberar recursos
+    if (close(fd_shm) == -1) {
+        perror("Error al cerrar la memoria compartida");
+        exit(EXIT_FAILURE);
+    }
+    if (shm_unlink(MC) == -1) {
+        perror("Error al eliminar la memoria compartida");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main() {
-    pthread_t hilos[N_THREADS];
-    int ids[N_THREADS];
-    sem_init(&semaforo, 0, 1); // inicializar el semáforo con valor 1
-    for (int i = 0; i < N_THREADS; i++) {
-        ids[i] = i + 1;
-        pthread_create(&hilos[i], NULL, funcion_hilo, (void *) &ids[i]);
-    }
-    for (int i = 0; i < N_THREADS; i++) {
-        pthread_join(hilos[i], NULL);
-    }
-    sem_destroy(&semaforo); // liberar el semáforo
-    printf("Valor final del contador: %d\n", contador);
+    int fd_shm;
+
+    // Crear la memoria compartida
+    crearMemoriaCompartida(&fd_shm);
+
+    printf("Memoria compartida creada correctamente\n");
+
+    // Realizar operaciones con la memoria compartida
+
+    // Borrar la memoria compartida
+    borrarMemoriaCompartida(fd_shm);
+
     return 0;
 }
